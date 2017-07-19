39b2e6f70b489429d823f3166c01f6f67b9ca610
GitHub-bigT <564241248@qq.com>
2017-07-19 13:55:14 +0800

    show a pic , change size

---
+++ opencv/showpic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <core.hpp>
+#include <imgproc.hpp>
+#include <highgui\highgui.hpp>
+
+using namespace cv;
+
+void main(){
+	Mat img = imread("pic.jpg");
+	Mat imgSize ;
+	resize(img,imgSize,Size(1000,700),0,0,CV_INTER_LINEAR);
+	namedWindow("picture");
+	imshow("pic",imgSize);
+	waitKey(60000);
+}
